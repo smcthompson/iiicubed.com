46a8c5ab9c7ad81150d9d7b6859ce9717a0ca53f
Scott Thompson <smcthompson@users.noreply.github.com>
2026-07-06 02:17:19 -0500

    Add workstation app module exports

---
+++ src/app/workstation/index.ts
@@ -0,0 +1,2 @@
+export { getWorkstationApplication } from '#/app/workstation/WorkstationApplication.js';
+export { workstationRoutes } from '#/app/workstation/presentation/routes/workstationRoutes.js';
