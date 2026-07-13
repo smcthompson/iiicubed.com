0685740b6e3dc80d1a277542a02c981e249fae16
Scott Thompson <iiicubed.com@gmail.com>
2026-07-12 22:19:28 -0500

    Added workstation barrels...
    ...to keep `import`s simply and consistent.
    
    Issue AB#33
    Task AB#34

---
+++ src/app/shared/index.ts
@@ -0,0 +1,3 @@
+export { nodeEnv } from '#/app/shared/config/loadEnvironment.js';
+export { readRequiredEnv, readOptionalEnv } from '#/app/shared/config/readRequiredEnv.js';
+export type { QueryExecutor, QueryParameters, QueryParameterValue, QueryResult } from '#/app/shared/database/QueryExecutor.js';
